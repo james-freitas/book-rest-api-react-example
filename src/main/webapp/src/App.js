cdcd4b08eecfabebf152814065b978270a829b7e
James Freitas <jamesfrj@yahoo.com.br>
2021-05-31 07:15:44 -0300

    Add Jumbotron

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import { Col, Container, Jumbotron, Row } from 'react-bootstrap';
 import './App.css';
 
 import NavigationBar from './components/NavigationBar'
 
 function App() {
+  const marginTop = {
+    marginTop: "20px"
+  };
   return (
     <div className="App">
       <NavigationBar/>
-      <p>
-        Welcome to Book Shop
-      </p>
+      <Container>
+        <Row>
+          <Col lg={12} style={marginTop}>
+            <Jumbotron className="bg-dark text-white">
+              <h1>Hello, world!</h1>
+              <p>
+                This is a simple hero unit, a simple jumbotron-style component for calling
+                extra attention to featured content or information.
+              </p>
+            </Jumbotron>
+          </Col>
+        </Row>  
+      </Container>
     </div>
   );
 }
