3167b9e6b02e8e49d0e215835c068fcdf5e47a49
James Freitas <jamesfrj@yahoo.com.br>
2021-07-03 13:42:32 -0300

    Add Routing

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer';
 
 import NavigationBar from './components/NavigationBar';
 import Welcome from './components/Welcome';
+import Book from './components/Book';
+import BookList from './components/BookList';
 
 function App() {
   const marginTop = {
     marginTop: "20px"
   };
   return (
-    <div className="App">
+    <Router>
       <NavigationBar/>
       <Container>
         <Row>
           <Col lg={12} style={marginTop}>
-            <Welcome/>
+            <Switch>
+              <Route path="/" exact component={Welcome}/>
+              <Route path="/add" exact component={Book}/>
+              <Route path="/list" exact component={BookList}/>
+            </Switch>
           </Col>
         </Row>  
       </Container>
       <Footer/>
-    </div>
+    </Router>
   );
 }
 
--- src/main/webapp/src/components/NavigationBar.js
+++ src/main/webapp/src/components/NavigationBar.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
 import {Nav, Navbar} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 class NavigationBar extends React.Component {
   render() {
     return (
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="/">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/b/ba/Book_icon_1.png" width="25" height="25" alt="brand" />  
-          Book Shop
-        </Navbar.Brand>
+        <Link to={""} className="navbar-brand">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/b/ba/Book_icon_1.png" width="25" height="25" alt="brand" />Book Shop
+        </Link>
         <Nav className="mr-auto">
-          <Nav.Link href="#">Add Book</Nav.Link>
-          <Nav.Link href="#">Book List</Nav.Link>
+          <Link to={"add"} className="nav-link">Add Book</Link>
+          <Link to={"list"} className="nav-link">Book List</Link>
         </Nav>
       </Navbar>
     );
