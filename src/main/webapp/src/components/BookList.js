39ebec6d4ef87bc2b0d14e231dc2a9f40cb9f719
James Freitas <jamesfrj@yahoo.com.br>
2021-07-08 06:18:10 -0300

    Add a table to the BookList

--- src/main/webapp/src/components/BookList.js
+++ src/main/webapp/src/components/BookList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Table } from 'react-bootstrap';
 
 
 export default class BookList extends Component {
@@ -8,7 +8,23 @@ export default class BookList extends Component {
       <Card className={"border border-dark bg-dark text-white"}>
         <Card.Header>Book List</Card.Header>
         <Card.Body>
-
+          <Table bordered hover striped variant="dark">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Author</th>
+                <th>ISBN Number</th>
+                <th>Price</th>
+                <th>Language</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+            <tr align="center">
+              <td colSpan="6">No Books Available.</td>
+            </tr>
+          </tbody>
+          </Table>
         </Card.Body>
       </Card>
     );
