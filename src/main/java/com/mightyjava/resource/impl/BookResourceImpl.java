dcbc5882efd8a6ab73417085397e63917dde0627
James Freitas <jamesfrj@yahoo.com.br>
2021-10-09 22:05:24 -0300

    frontend - Get all books feature using axios

--- src/main/java/com/mightyjava/resource/impl/BookResourceImpl.java
+++ src/main/java/com/mightyjava/resource/impl/BookResourceImpl.java
@@ -13,6 +13,7 @@ import org.codehaus.jettison.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +28,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequestMapping("/books")
+@CrossOrigin(origins = "http://localhost:3000")
 public class BookResourceImpl implements Resource<Book> {
 
 	@Autowired
--- src/main/webapp/src/components/BookList.js
+++ src/main/webapp/src/components/BookList.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
-import { Card, Table } from 'react-bootstrap';
+import { Card, Table, Image, Button, ButtonGroup } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faList } from '@fortawesome/free-solid-svg-icons';
+import { faEdit, faList, faTrash } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
 
 export default class BookList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      books : []
+    };
+  }
+
+  componentDidMount() {
+    this.findAllBooks();
+  }
+
+  findAllBooks() {
+    axios.get("http://localhost:8081/rest/books")
+      .then(response => response.data)
+      .then((data) => {
+        this.setState({books: data});
+      });
+  }
+
   render() {
     return (
       <Card className={"border border-dark bg-dark text-white"}>
@@ -21,8 +42,31 @@ export default class BookList extends Component {
               </tr>
             </thead>
             <tbody>
+              {
+                this.state.books.length === 0 ?
+                <tr align="center">
+                  <td colspan="6">No Books Available.</td>
+                </tr> :
+                this.state.books.map((book) => (
+                  <tr key={book.id}>
+                    <td>
+                      <Image src={book.coverPhotoURL} roundedCircle width="25" height="25"/> {book.title}
+                      </td>    
+                    <td>{book.author}</td>    
+                    <td>{book.isbnNumber}</td>    
+                    <td>{book.price}</td>    
+                    <td>{book.language}</td>
+                    <td>
+                      <ButtonGroup>
+                        <Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit} /></Button>{' '}
+                        <Button size="sm" variant="outline-danger"><FontAwesomeIcon icon={faTrash} /></Button>
+                      </ButtonGroup>
+                    </td>    
+                  </tr>
+                ))
+              }
             <tr align="center">
-              <td colSpan="6">No Books Available.</td>
+              <td colSpan="6">{this.state.books.length} Books Available.</td>
             </tr>
           </tbody>
           </Table>
